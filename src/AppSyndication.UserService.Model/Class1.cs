514e68f0701ce63b95210890580c6c0b0954139e
Rob Mensching <rob@robmensching.com>
2017-01-06 11:19:02 -0800

    Make project usable in VS

---
+++ src/AppSyndication.UserService.Model/Class1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AppSyndication.UserService.Model
+{
+    public class Class1
+    {
+        public Class1()
+        {
+        }
+    }
+}
